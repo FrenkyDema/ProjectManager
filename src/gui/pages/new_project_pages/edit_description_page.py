02f4d4fe7a1ea40b3324a2d5e828c7a3c7364c37
FrenkyDema <frenkydema@gmail.com>
2022-09-23 21:57:54 +0200

    Update edit_description_page.py
    refactoring

--- src/gui/pages/new_project_pages/edit_description_page.py
+++ src/gui/pages/new_project_pages/edit_description_page.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-__autor__ = "Francesco"
+__author__ = "Francesco"
 __version__ = "0101 2022/06/05"
+
 import tkinter
+from tkinter import scrolledtext
+
 from customtkinter import *
-from tkinter import messagebox, scrolledtext
 
-
-from ....lib import project_lib
 from ... import main_page_enum
-
+from ....lib import project_lib
 
 CONFIG_FILE = "config.json"
 PROJECT_SETTINGS_FILE = "project_settings.json"
+
 
 class EditDescriptionPage(CTkFrame):
     def __init__(self, master, app):
@@ -26,9 +27,8 @@ class EditDescriptionPage(CTkFrame):
                               text_font=("Roboto Medium", 20))  # font name and size in px
         self.title.grid(row=0, column=0, pady=10)
 
-
         frame = CTkFrame(master=self,
-                            corner_radius=10)
+                         corner_radius=10)
         frame.grid(row=1, column=0, padx=10, pady=10, sticky="nswe")
 
         # configure grid layout (3x7)
@@ -40,9 +40,9 @@ class EditDescriptionPage(CTkFrame):
         # create label on CTkToplevel self.description_window
 
         self.text_area = scrolledtext.ScrolledText(frame, wrap=tkinter.WORD,
-                                                font=("Roboto Medium", 10))
+                                                   font=("Roboto Medium", 10))
         self.text_area.insert(tkinter.INSERT,
-                            project_lib.get_key_value_JSON(PROJECT_SETTINGS_FILE, "descrizione").rstrip("\n"))
+                              project_lib.get_key_value_JSON(PROJECT_SETTINGS_FILE, "description").rstrip("\n"))
 
         self.text_area.grid(column=0, row=0, columnspan=3, pady=10, padx=10, sticky="nsew")
 
@@ -50,15 +50,15 @@ class EditDescriptionPage(CTkFrame):
         self.text_area.focus()
 
         self.clear_button = CTkButton(master=frame,
-                                    text="Cancella",
-                                    fg_color=("gray75", "gray30"),
-                                    command=self.clear_text)
+                                      text="Cancella",
+                                      fg_color=("gray75", "gray30"),
+                                      command=self.clear_text)
         self.clear_button.grid(row=1, column=0, pady=5, padx=5, sticky="sw")
 
         self.submit_button = CTkButton(master=frame,
-                                    text="Fatto",
-                                    fg_color=("gray75", "gray30"),
-                                    command=self.back)
+                                       text="Fatto",
+                                       fg_color=("gray75", "gray30"),
+                                       command=self.back)
         self.submit_button.grid(row=1, column=2, pady=5, padx=5, sticky="se")
 
     def clear_text(self):
@@ -68,6 +68,6 @@ class EditDescriptionPage(CTkFrame):
         description = self.text_area.get("1.0", tkinter.END).rstrip("\n")
 
         project_lib.update_key_JSON(
-            PROJECT_SETTINGS_FILE, "descrizione", description)
+            PROJECT_SETTINGS_FILE, "description", description)
 
         self.app.chose_frame(main_page_enum.MainPageEnum.NEW_PROJECT)
