7b1367ef186300c059da2249e74b1530325ada87
FrenkyDema <frenkydema@gmail.com>
2022-09-23 21:32:57 +0200

    Update main.py
    refactoring
    translation

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@ import sys
 import types
 from pathlib import Path
 
+import main_application
+from src.lib import project_lib
+
 
 def import_parents(level):
-    global __package__
     file = Path(__file__).resolve()
     parent, top = file.parent, file.parents[level]
 
     sys.path.append(str(top))
     try:
         sys.path.remove(str(parent))
-    except ValueError:  # giá rimosso
+    except ValueError:
         pass
 
     __package__ = '.'.join(parent.parts[len(top.parts):])
@@ -26,10 +28,6 @@ def print_import(string):
 
 
 import_parents(1)
-
-from .lib import project_lib
-
-from .gui import main_application
 
 if __name__ == "__main__":
     project_lib.default_project_settings_values()
